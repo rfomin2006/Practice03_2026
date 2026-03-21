e3eddf8b6fc50ffead23dabe88c203b753725016
rfomin2006 <rfomin2805@gmail.com>
2026-03-21 17:20:40 +0700

    Realised recursive factorial.

--- recursive_algorithms/factorial.c
+++ recursive_algorithms/factorial.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+int fact(int);
+
 int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d", fact(n));
     return 0;
+}
+
+int fact(int num) {
+    return num == 1 ? num : num * fact(num - 1);
 }
