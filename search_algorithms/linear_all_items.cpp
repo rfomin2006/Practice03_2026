13ce1e8584a0390e02a8c7837b6755ab11ae484f
rfomin2006 <rfomin2805@gmail.com>
2026-03-21 18:35:25 +0700

    implemented linear search of all occurrences. + fix.

--- search_algorithms/linear_all_items.cpp
+++ search_algorithms/linear_all_items.cpp
@@ -3,13 +3,13 @@
 #include <string>
 #include <sstream>
 
-int linear_search(std::vector<int> arr, int val);
+std::vector<int> linear_search(std::vector<int> arr, int val);
 
 int main() {
     std::vector<int> array;
     std::string line;
     int num;
-    int res;
+    std::vector<int> res;
     std::cout << "Enter array (one line, separated by spaces):" << std::endl;
     std::getline(std::cin, line);
     std::stringstream ss(line);
@@ -17,13 +17,22 @@ int main() {
     std::cout << "Enter element for search: ";
     std::cin >> num;
     res = linear_search(array, num);
-    std::cout << (res != -1 ? "Element index is " + std::to_string(res) : "Element not found!");
+    if (res.size() >= 1) {
+        std::cout << "Element indexes is: ";
+        for (const auto& el : res) {
+            std::cout << el << " ";
+        }
+        std::cout << std::endl;
+    } else {
+        std::cout << "Element not found!";
+    }
     return 0;
 }
 
-int linear_search(std::vector<int> arr, int val) {
+std::vector<int> linear_search(std::vector<int> arr, int val) {
+    std::vector<int> indexes;
     for (size_t i = 0; i < arr.size(); i++) {
-        if (arr[i] == val) return i; 
+        if (arr[i] == val) indexes.push_back(i); 
     }
-    return -1;
+    return indexes;
 }
